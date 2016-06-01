d892a79b929afccb9a9fd9b4d41e88c32d7bb05b
Brant Wellman <BrantWellman@gmail.com>
2016-06-01 16:11:08 -0600

    Add check user authentication
    Unauthenticated_user method, current_user method
    Portal view, links and sessions controller

---
+++ test/integration/unauthenticated_user_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UnauthenticatedUserTest < ActionDispatch::IntegrationTest
+  test "unauthenticated user redirected to login page" do
+    visit root_path
+
+    assert_equal portal_path, current_path
+    assert page.has_link('Log In')
+    assert page.has_link('Sign Up')
+  end
+end
