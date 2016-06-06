8a8b233893cd8e5591790d19960148a1d8653ebc
Brant Wellman <BrantWellman@gmail.com>
2016-06-06 14:02:42 -0600

    Add User Sign up functionality
    New user can sign up
    Email uniqueness and presence validations
    User logged in and redirected to Links index
    All tested

--- test/integration/unauthenticated_user_test.rb
+++ test/integration/unauthenticated_user_test.rb
@@ -18,6 +18,31 @@ class UnauthenticatedUserTest < ActionDispatch::IntegrationTest
     fill_in "Password confirmation", with: "password"
     click_on "Submit"
 
-    assert page.has_content 'All of the Links'
+    assert page.has_content?('All of the Links')
+  end
+
+  test "user can't sign up duplicate email" do
+    create(:user)
+
+    visit root_path
+
+    click_on "Sign Up"
+    fill_in "Email", with: "brant@email.com"
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_on "Submit"
+
+    assert page.has_content?("Email has already been taken")
+  end
+
+  test "user can't sign up with unmatched password and confirmation" do
+    visit new_user_path
+
+    fill_in "Email", with: "brant@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "unmatched"
+    click_on "Submit"
+
+    assert page.has_content?("Password confirmation doesn't match Password")
   end
 end
